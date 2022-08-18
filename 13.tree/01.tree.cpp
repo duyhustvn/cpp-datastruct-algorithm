c12b94c4218b63c2c52395d2340e487f51a5e6ac
duyhustvn <mac@Venders-MacBook-Pro.local>
2022-08-18 08:56:32 +0700

    height size max

--- 13.tree/01.tree.cpp
+++ 13.tree/01.tree.cpp
@@ -58,6 +58,57 @@ void DFSPostorder(Node *root) {
   }
 }
 
+int height(Node *root) {
+  if (root == NULL) {
+      return 0;
+  }
+
+  return max(height(root->left), height(root->right)) + 1;
+}
+
+int size(Node *root) {
+  int count = 0;
+  queue<Node*> q;
+  q.push(root);
+  while (!q.empty()) {
+    Node *currentNode = q.front();
+    q.pop();
+    count++;
+
+    if (currentNode->left != NULL) {
+      q.push(currentNode->left);
+    }
+    if (currentNode->right != NULL) {
+      q.push(currentNode->right);
+    }
+  }
+  return count;
+}
+
+int getSize(Node *root) {
+  if (root == NULL) {
+    return 0;
+  }
+  return getSize(root->left) + getSize(root->right) + 1;
+}
+
+void nodeAtDistanceK(Node *root, int k) {
+  if (root == NULL) return;
+
+  if (k == 0) {
+    cout << root->key << " ";
+  }
+
+    nodeAtDistanceK(root->left, k-1);
+    nodeAtDistanceK(root->right, k-1);
+}
+
+int getMax(Node *root) {
+  if (root != NULL) {
+    return max(root->key, max(getMax(root->left), getMax(root->right)));
+  }
+  return INT_MIN;
+}
 
 int main() {
   /*
@@ -95,6 +146,40 @@ int main() {
   cout << "DFSPostorder" << endl;
   DFSPostorder(root);
   cout << endl;
+
+  cout << "height of tree: " << height(root) << endl;
+
+  int k = 0;
+  cout << "node at distance " << k << " are: ";
+  nodeAtDistanceK(root, k);
+  cout << endl;
+
+
+  k = 1;
+  cout << "node at distance " << k << " are: ";
+  nodeAtDistanceK(root, k);
+  cout << endl;
+
+  k = 2;
+  cout << "node at distance " << k << " are: ";
+  nodeAtDistanceK(root, k);
+  cout << endl;
+
+
+  k = 3;
+  cout << "node at distance " << k << " are: ";
+  nodeAtDistanceK(root, k);
+  cout << endl;
+
+  cout << "Size of the tree: ";
+  cout << size(root) << endl;
+
+
+  cout << "Size of the tree: ";
+  cout << getSize(root) << endl;
+
+  cout << "Max: ";
+  cout << getMax(root) << endl;
   return 0;
 }
 
