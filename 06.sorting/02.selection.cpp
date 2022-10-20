aa26eb42d51042bd739ca34041548c60df864b35
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2022-10-20 17:40:14 +0700

    selection sort

---
+++ 06.sorting/02.selection.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+    void swap(int arr[], int i, int j) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+
+    void selectionSort(int arr[], int n)
+    {
+       for (int i = 0; i < n; i++) {
+        int min = arr[i];
+        int minIndex = i;
+
+        for (int j = i; j < n; j++) {
+          if (arr[j] < min) {
+            min = arr[j];
+            minIndex = j;
+          }
+        }
+
+        if (minIndex != i) {
+          swap(arr, i, minIndex);
+        }
+      }
+
+    }
+};
+
+void printArray(int nums[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << nums[i] << " ";
+  }
+
+  cout << endl;
+}
+
+int main() {
+  Solution *s = new Solution();
+  int nums1[] = {1,3,7,2,6,4,5};
+  int n1 = sizeof(nums1)/sizeof(nums1[0]);
+  s->selectionSort(nums1, n1);
+  printArray(nums1, n1);
+}
