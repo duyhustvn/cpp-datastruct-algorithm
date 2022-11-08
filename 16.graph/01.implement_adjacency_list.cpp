121a2b0276d4c0e9aaec400a42e610fef7a0bf07
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2022-11-08 11:11:22 +0700

    bfs

--- 16.graph/01.implement_adjacency_list.cpp
+++ 16.graph/01.implement_adjacency_list.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <unordered_map>
 
 using namespace std;
 
@@ -7,11 +9,13 @@ class UnDirectedGraph {
   friend class UnDirectedGraphTest;
 
   FRIEND_TEST(UnDirectedGraphTest, MethodAdd);
+  FRIEND_TEST(UnDirectedGraphTest, MethodBFS);
 
   private:
   int V;
   vector<vector<int>> adj;
 
+  // use for testing only
   vector<vector<int>> getAdj() {
     return adj;
   }
@@ -38,5 +42,30 @@ class UnDirectedGraph {
     }
   }
 
-};
+  vector<int> BFS(int source) {
+    vector<int> result;
+    deque<int> q;
+    q.push_back(source);
+    unordered_map<int, bool> visited;
+    visited[source] = true;
 
+    int curVertex;
+    while (!q.empty()) {
+      curVertex = q.front();
+      result.push_back(curVertex);
+      q.pop_front();
+
+      vector<int> neighbors = adj[curVertex];
+      for (auto neighbor: neighbors) {
+        // not visited
+        if (visited.find(neighbor) == visited.end()) {
+          visited[neighbor] = true;
+          q.push_back(neighbor);
+
+        }
+      }
+    }
+    cout << endl;
+    return result;
+  }
+};
--- 16.graph/01.implement_adjacency_list_test.cpp
+++ 16.graph/01.implement_adjacency_list_test.cpp
@@ -32,6 +32,55 @@ TEST_F(UnDirectedGraphTest, MethodAdd) {
   graph->printGraph(5);
 }
 
+TEST_F(UnDirectedGraphTest, MethodBFS) {
+  UnDirectedGraph *graph;
+  vector<int> realResult;
+  vector<int> expectedResult;
+
+  graph = new UnDirectedGraph(6);
+  graph->add(0,1);
+  graph->add(0,2);
+  graph->add(0,5);
+  graph->add(1,3);
+  graph->add(2,4);
+  graph->add(3,5);
+  graph->add(4,5);
+
+  // graph->printGraph(6);
+  realResult = graph->BFS(0);
+
+  expectedResult = {0,1,2,5,3,4};
+  ASSERT_EQ(realResult, expectedResult);
+
+  graph = new UnDirectedGraph(5);
+  graph->add(0,1);
+  graph->add(0,2);
+  graph->add(1,2);
+  graph->add(1,3);
+  graph->add(2,3);
+  graph->add(2,4);
+  graph->add(3,4);
+
+  realResult = graph->BFS(0);
+  expectedResult = {0,1,2,3,4};
+  ASSERT_EQ(realResult, expectedResult);
+
+
+  realResult = graph->BFS(2);
+  expectedResult = {2,0,1,3,4};
+  ASSERT_EQ(realResult, expectedResult);
+
+
+  realResult = graph->BFS(1);
+  expectedResult = {1,0,2,3,4};
+  ASSERT_EQ(realResult, expectedResult);
+
+
+  realResult = graph->BFS(4);
+  expectedResult = {4,2,3,0,1};
+  ASSERT_EQ(realResult, expectedResult);
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
 
