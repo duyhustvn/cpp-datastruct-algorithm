f19f3c1cff33d998b61e94f8177f1341d00c11ae
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2023-07-28 22:36:09 +0700

    pointer of structure

---
+++ 01.structure/04.struct_pointer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+  struct book {
+    char n[20];
+    int nop;
+    float pr;
+  };
+  struct book b = {"Basic", 425, 135.00};
+  struct book *ptr = &b;
+  printf("%s %d %f\n", b.n, b.nop, b.pr);
+  printf("%s %d %f\n", ptr->n, ptr->nop, ptr->pr);
+
+  // pointer to array of structure
+  struct employee {
+    char name;
+    int age;
+    float salary;
+  };
+
+  struct employee ems[] = {
+      {'A', 23, 400.5},
+      {'B', 24, 300.5},
+      {'C', 25, 500.5},
+  };
+
+  // pointer to char
+  char *p = (char *)ems;
+  // pointer to structure
+  struct employee *q = ems;
+  // pointer to an array of structure
+  struct employee(*r)[3] = (struct employee(*)[3])ems;
+
+  // Common programming error: z = e is wrong because an array name can never
+  // occur on the left-hand side of =.
+  // You cannot reassign the array name to point to a different memory location
+  // struct employee *z[3] = ems;
+
+  printf("%u\n", p);
+  printf("%u\n", q);
+  printf("%u\n", r);
+  p++;
+  q++;
+  r++;
+  printf("%u\n", p);
+  printf("%u\n", q);
+  printf("%u\n", r);
+
+  return 0;
+}
